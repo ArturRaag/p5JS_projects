f9be90a4a32c0456a87616fb1e01b9cede5eb4c0
Artur Raag <68288831+ArturRaag@users.noreply.github.com>
2022-10-10 23:03:46 +0300

    Update sketch_9_flower_with_sliders.js

--- 3D Math Rose model/sketch_9_flower_with_sliders/sketch_9_flower_with_sliders.js
+++ 3D Math Rose model/sketch_9_flower_with_sliders/sketch_9_flower_with_sliders.js
@@ -1,5 +1,3 @@
-
-
 function setup() {
   createCanvas(800,  800, WEBGL);
   angleMode(DEGREES);
@@ -9,28 +7,34 @@ function setup() {
   
   createDiv();
   nurk_phi_txt=createP("Nurk phi");
-  nurk_phi_txt.style("margin-right: 20px");
+  nurk_phi_txt.position(width+50,20)
   lehekeste_kõrgus=createSlider(1,25,15);
+  lehekeste_kõrgus.position(width+50,50);
+  
   
   createDiv();
   lõike_sügavus_txt=createP("Lehe lõike sügavus");
-  lõike_sügavus_txt.style("margin-right: 20px");
+  lõike_sügavus_txt.position(width+50,100);
   lõike_sügavus=createSlider(1,14,3);
+  lõike_sügavus.position(width+50,130);
   
   createDiv();
   lehe_otsad_txt=createP("Lehe otsa kuju");
-  lehe_otsad_txt.style("margin-right: 20px");
+  lehe_otsad_txt.position(width+50,200);
   lehe_otsad=createSlider(1,4,2);
+  lehe_otsad.position(width+50,230);
 
   createDiv();
   lehe_arv_txt=createP("Lehtede arv ja/või kattuvus");
-  lehe_arv_txt.style("margin-right: 20px");
+  lehe_arv_txt.position(width+50,300);
   lehe_arv=createSlider(1,25,6,0.1);
+  lehe_arv.position(width+50,330);
   
   createDiv();
   rippumise_slider_txt=createP("Lehe rippumine");
-  rippumise_slider_txt.style("margin-right: 20px");
+  rippumise_slider_txt.position(width+50,400);
   rippumise_slider=createSlider(-4,6,2,0.1);
+  rippumise_slider.position(width+50,430);
   
 }
 
